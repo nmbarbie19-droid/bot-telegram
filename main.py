4addd440b92fddf3729f62016b38963ef6f03cec
nmbarbie19-droid <nmbarbie19@gmail.com>
2026-02-20 03:37:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-import os
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+user_id = update.effective_user.id
+estado = usuarios.get(user_id)
 
-TOKEN = os.getenv("TOKEN")
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot está funcionando 🚀")
-
-if __name__ == "__main__":
-    app = ApplicationBuilder().token(TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
-    print("Bot iniciado...")
-    app.run_polling()
+if estado == "aguardando_pagamento":
+    await context.bot.send_message(
+        chat_id=user_id,
+        text="⏳ Seu acesso ainda está disponível.\nQuer garantir antes que o valor aumente?"
+    )
