70cbdb0900affd9be2a50034539888c9536e817f
nmbarbie19-droid <nmbarbie19@gmail.com>
2026-02-20 02:33:06 -0300

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+TOKEN = os.getenv("TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Bot está funcionando 🚀")
+
+if __name__ == "__main__":
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    print("Bot iniciado...")
+    app.run_polling()
